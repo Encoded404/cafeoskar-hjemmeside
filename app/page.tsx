86e650a1eec39cd05dc7cc6d5a71d62a0502e881
Encoded <75113441+Encoded404@users.noreply.github.com>
2024-10-28 16:06:09 +0100

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,76 +1,131 @@
 import Image from "next/image";
 import "./globals.css";
 import { request } from "http";
-import { ReactElement } from "react";
+import { AwaitedReactNode, JSXElementConstructor, ReactElement, ReactNode, ReactPortal } from "react";
 
-let mainPage = require("./public/mainPage.json")
+import mainPage from './public/texts/mainPage.json'
+import oskarCafeLogo from './public/logo-oskar.png'
 
 type StringArray = {
   Json: any;
   JsonName: string;
 };
 
-function ArrayToText(array: any[])
+function ArrayToText(array: any[], arrayParent: any[])
 {
-  const returnValue: any[] = array.map(value => [
-    <p style={{textAlign: "center"}}>{value}</p>
-  ])
-
+  let returnValue: any[] = []
+  let hasList: boolean = false;
+  returnValue = array.map(value => {
+    let returnValue: any[] = []
+    if(value.substring(0,1) == "\\" && value.substring(value.length-1) == "\\")
+    {
+      hasList = true;
+      returnValue = [
+        <ul style={{textAlign: "center", marginBottom: "1.5vh"}}>{arrayParent[value.substring(1, value.length-1)].map((arrayParentValue: string) => [<li>{arrayParentValue}</li>])}</ul>
+      ]
+    }
+    else
+    {
+      returnValue = [
+        <p style={{textAlign: "center", marginBottom: "1vh"}}>{value}</p>
+      ]
+    }
+    return returnValue
+  })
+  //console.log("return: "+returnValue)
   return (
     returnValue
   )
 }
-
-function Section()
+interface sectionInfo {
+  "name": string,
+  "id": number,
+  "text": string[],
+  "array"?: any[]
+}
+interface sectionProb {
+  section: sectionInfo
+}
+function Section({section}:sectionProb/*props: sectionProb/*section: { name: string; id: number; text: string[]; }[]*/)
 {
-  let section = mainPage.section.BilletOgBord;
+  //let section = mainPage.section.BilletOgBord;
+  //let section = props.section;
   //console.log(section)
   let textSection: any[] = []
+  
+  //console.log(section)
 
   let i: number = 0
-  for (let key in section) {
-    textSection[i] = section[key]
+  //for (let thing in section) {
+    textSection[i] = section
     let stringAr: String[]
-    //textSection["text"].forEach(element => {
-    //  
-    //});
-    //console.log(textSection[i])
     i++;
-  }
-  
-  const textContent: any[] = textSection.map(singleContent => [
-    <li key={singleContent.id} style={{fontSize: 24, textAlign: "center"}}>{singleContent.name}</li>,
-    <li key={singleContent.id}>
-      { ArrayToText(singleContent.text) }
-    </li>
-  ]);
+  //}
+
+  const textContent: JSX.Element[][] = textSection.map((singleContent) => {
+    let result = [<li key={singleContent.id * 2} style={{fontSize: 24, textAlign: "center"}}><h3>{singleContent.name}</h3></li>]
+    let addition: JSX.Element[] = []
+    addition = [<li key={singleContent.id * 2}>{ArrayToText(singleContent.text, singleContent)}</li>]
+    /*singleContent.text.forEach((element: string) => {
+      if(element.substring(0,1) == "\\" && element.substring(element.length-1) == "\\")
+      {
+        console.log(element.substring(0,1) + element.substring(element.length-1))
+        addition = addition.concat([
+          <li key={singleContent.id * 2 + 1}>
+            { ArrayToText(singleContent.text, {id: singleContent.id, name: singleContent.name, text: singleContent.text, array: [singleContent[element.substring(2, element.length-3)]] }) }
+          </li>
+        ])
+      }
+      else
+      {
+        addition = addition.concat([
+          <li key={singleContent.id * 2 + 1}>
+            { ArrayToText(singleContent.text, {id: singleContent.id, name: singleContent.name, text: singleContent.text }) }
+          </li> ])
+        console.log("normal-adding: "+addition[addition.length])
+      }
+    });*/
+    
+    result = result.concat(addition)
+    return result
+  });
   for(let kes in textContent)
   {
     //console.log(textContent[kes])
-    console.log(textContent[kes][1].props)
+    //console.log(textContent[kes][1].props)
   }
   //console.log(textContent)
   return (
-    <div>
-      <ul>
-        {textContent}
-      </ul>
-    </div>
+    <ul className="Flexbox" style={{width: "55vw"}}>
+      {textContent}
+    </ul>
   );
 }
 
 export default function Home() {
   return (
     <div className="Flexbox" style={{paddingTop: '50px', justifyContent: "center"}}>
-      <Image
-        src="/logo-oskar.png"
-        width={1000}
-        height={760}
-        className="hidden md:block"
-        alt="idk tetst"
-      />
-      <div>
-        <Section/>
+      <h1>
+        <Image
+          src={oskarCafeLogo}
+          width={1000}
+          height={760}
+          className="hidden md:block"
+          alt="idk tetst"
+        />
+      </h1>
+      <div style={{textWrap: "balance"}}>
+        <div style={{marginBottom: "4vh"}}>
+          <Section section={mainPage.section.omOskarCafe[0]} />
+        </div>
+        <div>
+          <Section section={mainPage.section.BilletOgBord[0]}/>
+          <Section section={mainPage.section.BilletOgBord[1]}/>
+          <Section section={mainPage.section.omOskarCafe[2]}/>
+        </div>
+        <div>
+          <Section section={mainPage.section.omOskarCafe[1]}/>
+        </div>
       </div>
     </div>
   );
